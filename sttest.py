40aed10cdb8b9176cf685a9e95c7250c96776a06
Sana <sanatays01@gmail.com>
2023-07-26 19:48:17 +0200

    Update streamlit

--- sttest.py
+++ sttest.py
@@ -1,12 +1,39 @@
+# import pandas as pd
+# import requests
+
+# import streamlit as st
+
+# text = st.text_input(label = "Enter your question : ")
+# res = requests.get("https://app-api-imlp5.herokuapp.com/api/text=" + ''.join(text))
+
+# res = pd.read_json(res.content.decode('utf-8')).loc[0, 'tags']
+
+# st.write(res)
+
 import pandas as pd
 import requests
 
 import streamlit as st
 
+st.title("My Streamlit Application")
 
-text = st.text_input(label = "Enter your question : ")
-res = requests.get("https://app-api-imlp5.herokuapp.com/api/text=" + ''.join(text))
+try:
+    user_input = st.text_input(label="Enter your question:")
 
-res = pd.read_json(res.content.decode('utf-8')).loc[0, 'tags']
+    if not user_input:
+        raise ValueError("Text cannot be empty")
 
-st.write(res)
+    api_url = "http://127.0.0.1:5000/api/text=" + user_input
+
+    response = requests.get(api_url)
+
+    if response.ok:
+	    data = response.json()
+	    tags = data["tags"]
+	    tag_names = [tag.strip("#") for tag in tags]
+	    st.write("Tags:", ", ".join(tag_names))
+    else:
+        raise ValueError("API request failed")
+
+except ValueError as e:
+    st.error(str(e))
