099903f7536de6cfec7be5bcb5738c706f0553df
Sana <sanatays01@gmail.com>
2023-07-26 19:33:29 +0200

    Revert "déploiement api flask"
    This reverts commit aeb4084fd21b3fd4be2443896ec075f648c9d429.

--- sttest.py
+++ sttest.py
@@ -1,40 +1,12 @@
-# import pandas as pd
-# import requests
-
-# import streamlit as st
-
-
-# text = st.text_input(label = "Enter your question : ")
-# res = requests.get("http://127.0.0.1:5000/api/text=" + ''.join(text))
-
-# res = pd.read_json(res.content.decode('utf-8')).loc[0, 'tags']
-
-# st.write(res)
-
 import pandas as pd
 import requests
 
 import streamlit as st
 
-st.title("My Streamlit Application")
 
-try:
-    user_input = st.text_input(label="Enter your question:")
+text = st.text_input(label = "Enter your question : ")
+res = requests.get("https://app-api-imlp5.herokuapp.com/api/text=" + ''.join(text))
 
-    if not user_input:
-        raise ValueError("Text cannot be empty")
+res = pd.read_json(res.content.decode('utf-8')).loc[0, 'tags']
 
-    api_url = "https://app-api-imlp5-2ba6f34a7807.herokuapp.com/api/text=" + user_input
-
-    response = requests.get(api_url)
-
-    if response.ok:
-	    data = response.json()
-	    tags = data["tags"]
-	    tag_names = [tag.strip("#") for tag in tags]
-	    st.write("Tags:", ", ".join(tag_names))
-    else:
-        raise ValueError("API request failed")
-
-except ValueError as e:
-    st.error(str(e))
+st.write(res)
